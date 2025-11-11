bcb194b522c894bddacd184cb72969157647cdc1
MusangQim <musangqim03@gmail.com>
2025-11-11 21:41:45 +0800

    add file memset

---
+++ 08-ft_memset/ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/11 21:38:41 by adzmusta          #+#    #+#             */
+/*   Updated: 2025/11/11 21:41:27 by adzmusta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memset(void memory, int value, size_t bytes)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)memory;
+	i = 0;
+	while (i < bytes)
+	{
+		ptr[i] = (unsigned char)value;
+		i++;
+	}
+	return (memory);
+}
