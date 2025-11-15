2571bc0a04bea9b37b65fc15dca1051346364d65
MusangQim <musangqim03@gmail.com>
2025-11-16 01:50:00 +0800

    modified file

--- 10-ft_memcpy/ft_memcpy.c
+++ 10-ft_memcpy/ft_memcpy.c
@@ -6,14 +6,27 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 21:50:25 by adzmusta          #+#    #+#             */
-/*   Updated: 2025/11/11 21:56:32 by adzmusta         ###   ########.fr       */
+/*   Updated: 2025/11/16 01:49:42 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void memory, int value, size_t bytes)
+void	*ft_memcpy(void *destination, const void *source, size_t bytes)
 {
-	size_t			i;
-	unsigned char 	*ptr;
+	const unsigned char	*src;
+	unsigned char		*dst;
+	size_t				i;
 
+	src = (const unsigned char *)source;
+	dst = (unsigned char *)destination;
+	if (!dst && !src)
+		return (NULL);
+	i = 0;
+	while (i < bytes)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	return (destination);
+}
