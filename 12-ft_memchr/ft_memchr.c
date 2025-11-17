7f505c60ff7b957b01cbb4685eba7fb4f6d5ae24
MusangQim <musangqim03@gmail.com>
2025-11-17 11:38:06 +0800

    fix error

--- 12-ft_memchr/ft_memchr.c
+++ 12-ft_memchr/ft_memchr.c
@@ -6,22 +6,24 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 15:45:30 by adzmusta          #+#    #+#             */
-/*   Updated: 2025/11/16 16:42:09 by adzmusta         ###   ########.fr       */
+/*   Updated: 2025/11/17 11:37:49 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(void *memory, const void *search, size_t bytes)
+void	*ft_memchr(const void *memory, int search, size_t bytes)
 {
-	unsigned char	*ptr;
-	size_t			i;
+	const unsigned char	*ptr;
+	unsigned char		find;
+	size_t				i;
 
-	ptr = (unsigned char *)memory;
+	ptr = (const unsigned char *)memory;
+	find = (unsigned char)search;
 	i = 0;
 	while (i < bytes)
 	{
-		if (ptr[i] == (unsigned char)search)
+		if (ptr[i] == find)
 			return ((void *)(ptr + i));
 		i++;
 	}
