5bb2d42c0b57f51ce84562dd4d95cecbcffa5390
MusangQim <musangqim03@gmail.com>
2025-11-17 15:28:15 +0800

    add files

---
+++ 14-ft_calloc/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 13:29:54 by adzmusta          #+#    #+#             */
+/*   Updated: 2025/11/17 15:28:03 by adzmusta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
