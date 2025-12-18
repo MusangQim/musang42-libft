b09e6ed1594d5af54a3a72fed9f1af35eece58d9
MusangQim <musangqim03@gmail.com>
2025-12-18 16:32:19 +0800

    add file strlcpy

---
+++ 18-ft_strlcpy/ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 16:11:44 by adzmusta          #+#    #+#             */
+/*   Updated: 2025/12/18 16:32:00 by adzmusta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
+{
+	size_t	i;
+	size_t	src_len;
+
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+	if (destsize == 0)
+		return (src_len);
+	i = 0;
+	while (src[i] && i < destsize - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
