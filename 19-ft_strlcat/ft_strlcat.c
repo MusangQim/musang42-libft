b58d75fbc11190f54d4c19eb5b5824b4aec59fb6
MusangQim <musangqim03@gmail.com>
2025-12-21 23:48:14 +0800

    add file strlcat

---
+++ 19-ft_strlcat/ft_strlcat.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/21 23:46:29 by adzmusta          #+#    #+#             */
+/*   Updated: 2025/12/21 23:47:54 by adzmusta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
+{
+	size_t	i;
+	size_t	src_len;
+	size_t	dst_len;
+
+	src_len = 0;
+	dst_len = 0;
+	
