189945d53fad0422cf068986f431cc16c9dc5ab0
MusangQim <musangqim03@gmail.com>
2025-12-24 23:52:59 +0800

    add file strchr

---
+++ 20-ft_strchr/ft_strchr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/24 23:50:47 by adzmusta          #+#    #+#             */
+/*   Updated: 2025/12/24 23:52:37 by adzmusta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int ch)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == (char)ch)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (str[i] == 
