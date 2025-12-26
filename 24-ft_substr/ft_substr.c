85f526f663701ebaaaa2d5ac7b9d2426a6a7ba88
MusangQim <musangqim03@gmail.com>
2025-12-26 22:55:56 +0800

    add file substr

---
+++ 24-ft_substr/ft_substr.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/26 22:31:09 by adzmusta          #+#    #+#             */
+/*   Updated: 2025/12/26 22:55:40 by adzmusta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	*str_empty(void)
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+
+}
+
