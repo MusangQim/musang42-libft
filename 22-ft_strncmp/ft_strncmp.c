c4e9e079b0f831372fce2332295e90699fe26845
MusangQim <musangqim03@gmail.com>
2025-12-26 18:22:32 +0800

    add file strncmp

---
+++ 22-ft_strncmp/ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/26 18:06:44 by adzmusta          #+#    #+#             */
+/*   Updated: 2025/12/26 18:22:13 by adzmusta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	string_1;
+	unsigned char	string_2;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		string_1 = unsigned char s1[i];
+		string_2 = unsigned char s2[i];
+		if (string_1 != string_2)
+			return (string_1 - string_2);
+		i++;
+	}
+	return (0);
+}
