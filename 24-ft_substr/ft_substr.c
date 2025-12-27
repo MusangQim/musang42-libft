f0eb3b1fd7da818921b735d7761d05fef4e5ae11
MusangQim <musangqim03@gmail.com>
2025-12-27 21:43:27 +0800

    update file

--- 24-ft_substr/ft_substr.c
+++ 24-ft_substr/ft_substr.c
@@ -6,13 +6,19 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 22:31:09 by adzmusta          #+#    #+#             */
-/*   Updated: 2025/12/26 22:55:40 by adzmusta         ###   ########.fr       */
+/*   Updated: 2025/12/27 21:43:14 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 static char	*str_empty(void)
+{
+	char *string;
+
+	string = malloc(1);
+
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
