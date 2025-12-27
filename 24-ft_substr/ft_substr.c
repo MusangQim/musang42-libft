fff7f541188c6321e1cacad2fd38e175e2369b6f
MusangQim <musangqim03@gmail.com>
2025-12-27 22:30:14 +0800

    update file

--- 24-ft_substr/ft_substr.c
+++ 24-ft_substr/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 22:31:09 by adzmusta          #+#    #+#             */
-/*   Updated: 2025/12/27 22:19:18 by adzmusta         ###   ########.fr       */
+/*   Updated: 2025/12/27 22:29:01 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,45 @@
 
 static char	*str_empty(void)
 {
-	char	*string;
+	char	*str;
 
-	string = malloc(1);
-	if (!string)
+	str = malloc(1);
+	if (!str)
 		return (NULL);
-	*string = '\0';
-	return (string);
+	*str = '\0';
+	return (str);
 }
 
 static char	*str_new(char const *s, size_t start, size_t len)
 {
-	char		*string;
+	char		*str;
 	size_t		i;
-	
-	string = malloc(len + 1);
-	
 
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t		substr_len;
-	size_t		i;
-	char		*str_new;
+	size_t	substr_len;
+	size_t	real_len;
 
-	if (s == NULL)
+	if (!s)
 		return (NULL);
+	substr_len = ft_strlen(s);
 	if (start >= ft_strlen(s))
 		return (str_empty());
-	substr_len = ft_strlen(s) - start;
-	if (substr_len > len)
-		substr_len = len;
+	real_len = substr_len - start;
+	if (real_len > len)
+		real_len = len;
+	return (str_new(s, start, real_len));
 }
-
