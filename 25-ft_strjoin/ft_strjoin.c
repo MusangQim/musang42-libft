3b7456b6e401d844a1719d69d357a8a6ef0e56da
MusangQim <musangqim03@gmail.com>
2025-12-28 23:40:00 +0800

    update file

--- 25-ft_strjoin/ft_strjoin.c
+++ 25-ft_strjoin/ft_strjoin.c
@@ -6,10 +6,13 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 23:37:53 by adzmusta          #+#    #+#             */
-/*   Updated: 2025/12/28 23:38:35 by adzmusta         ###   ########.fr       */
+/*   Updated: 2025/12/28 23:39:50 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t		total_len;
+	char		*new_str;
