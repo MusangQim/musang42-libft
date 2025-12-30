026e96ad2722f4cc59961de2cab0733dee45361e
MusangQim <musangqim03@gmail.com>
2025-12-30 23:49:25 +0800

    update file

--- 27-ft_split/ft_split.c
+++ 27-ft_split/ft_split.c
@@ -6,11 +6,22 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 23:42:52 by adzmusta          #+#    #+#             */
-/*   Updated: 2025/12/30 23:43:54 by adzmusta         ###   ########.fr       */
+/*   Updated: 2025/12/30 23:49:11 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+static int	count_word(char const *s, char c)
+{
+}
+
+static int	len_word(const char *s, char c)
+{
+}
+
+static void	free_word
+{
+}
 
 char	**ft_split(char const *s, char c)
 {
