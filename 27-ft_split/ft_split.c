9d74ee799681ff2492e73c12d5bd47f826154242
MusangQim <musangqim03@gmail.com>
2026-01-01 21:58:58 +0800

    update file

--- 27-ft_split/ft_split.c
+++ 27-ft_split/ft_split.c
@@ -6,30 +6,45 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 23:42:52 by adzmusta          #+#    #+#             */
-/*   Updated: 2025/12/31 12:16:59 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/01 21:58:44 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-static int	count_word(char const *s, char c)
+static size_t	count_word(char const *s, char c)
+{
+	int	i;
+	int	count;
+
+	count = 0;
+	i = 0;
+	while (s[i] == 0)
+		return (0);
+	while (s[i] != c && (i == 0 || s[i - 1] == c))
+		count++;
+	return (count);
+}
+
+static size_t	len_word(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] = '\0')
+		return (NULL);
+	while (s[i] == c || s[i] == '\0')
+		i++;
+	return (i);
+}
+
+static char	*copy_word(char const *start, size_t len)
 {
 }
 
-static int	len_word(const char *s, char c)
-{
-}
-
-static void	free_word
+static void	free_word(char **arr, size_t used)
 {
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int	i;
-	int	len;
-	char		**result;
-
-	if (!s)
-		return (NULL);
-	result = 
 }
