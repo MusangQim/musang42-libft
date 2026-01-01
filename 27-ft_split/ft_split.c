96b83f2f0c9d54792554814ac456db08535ef074
MusangQim <musangqim03@gmail.com>
2026-01-01 23:14:12 +0800

    update error

--- 27-ft_split/ft_split.c
+++ 27-ft_split/ft_split.c
@@ -6,11 +6,12 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 23:42:52 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/01 22:32:45 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/01 23:13:51 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 static size_t	count_word(char const *s, char c)
 {
 	size_t	count;
@@ -33,7 +34,7 @@ static size_t	len_word(const char *s, char c)
 {
 	size_t	i;
 
-	while (!s)
+	if (!s)
 		return (0);
 	i = 0;
 	while (s[i] != c && s[i] != '\0')
@@ -43,10 +44,35 @@ static size_t	len_word(const char *s, char c)
 
 static char	*copy_word(char const *start, size_t len)
 {
+	char		*word;
+	size_t		i;
+
+	word = malloc(len + 1);
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = start[i];
+		i++;
+	}
+	word[len] = '\0';
+	return (word);
 }
 
-static void	free_word(char **arr, size_t used)
+static char	free_word(char **arr, size_t used)
 {
+	size_t	i;
+
+	i = 0;
+	while (i < used)
+	{
+		if (arr[i])
+			free(arr[i]);
+		i++;
+	}
+	free(arr);
+	return (NULL);
 }
 
 char	**ft_split(char const *s, char c)
