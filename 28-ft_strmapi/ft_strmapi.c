ad5d0dd1d0b537bb727c82e165e51f6f9c15c36e
MusangQim <musangqim03@gmail.com>
2026-01-02 23:20:07 +0800

    update file

--- 28-ft_strmapi/ft_strmapi.c
+++ 28-ft_strmapi/ft_strmapi.c
@@ -6,10 +6,18 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 23:16:49 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/02 23:17:45 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/02 23:19:55 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char		*new_str;
+	unsigned int	i;
+
+	if (!s || !f)
+		return (NULL);
+	new_str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+
