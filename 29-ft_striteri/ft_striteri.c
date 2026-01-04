31a7cc65fbdede200b475e079ec568ff4db8462e
MusangQim <musangqim03@gmail.com>
2026-01-04 21:05:07 +0800

    update file

--- 29-ft_striteri/ft_striteri.c
+++ 29-ft_striteri/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 21:02:13 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/04 21:03:55 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/04 21:04:50 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,10 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 
 	if (!s || !f)
 		return ;
-
-
+	i = 0;
+	while (s[i] != '\0')
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
