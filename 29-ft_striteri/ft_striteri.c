ddd54c30c462387f2e2dfee0d6d9f1aa1007040c
MusangQim <musangqim03@gmail.com>
2026-01-04 21:04:06 +0800

    update file

--- 29-ft_striteri/ft_striteri.c
+++ 29-ft_striteri/ft_striteri.c
@@ -6,8 +6,17 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 21:02:13 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/04 21:02:14 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/04 21:03:55 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return ;
 
 
