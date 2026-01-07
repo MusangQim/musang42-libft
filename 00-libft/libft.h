69712376d89941d61edd411188856a2576fdb396
MusangQim <musangqim03@gmail.com>
2026-01-07 22:05:27 +0800

    update file

--- 00-libft/libft.h
+++ 00-libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 21:37:11 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/07 21:54:24 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/07 22:05:15 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
+char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *str, int ch);
 char	*ft_strrchr(const char *str, int ch);
-char	*ft_strnstr(const char *s1, const char *s2, size len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strnstr(const char *s1, const char *s2, size length);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
