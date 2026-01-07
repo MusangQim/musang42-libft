d004ea8206680cb93c603f6172a3f98e028fa317
MusangQim <musangqim03@gmail.com>
2026-01-07 21:38:57 +0800

    update file

--- 00-libft/libft.h
+++ 00-libft/libft.h
@@ -6,8 +6,17 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 21:37:11 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/07 21:37:13 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/07 21:38:45 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
